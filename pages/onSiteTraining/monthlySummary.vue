75bf86db4b69a1045a160dcead5dc66811b66c4f
GGSMD <qweasdzxc102@qq.com>
2020-11-04 10:21:03 +0800

    提交

---
+++ pages/onSiteTraining/monthlySummary.vue
@@ -0,0 +1,20 @@
+<!-- 月汇总 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
